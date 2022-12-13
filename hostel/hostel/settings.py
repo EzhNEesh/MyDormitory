4c2852afa85e83af8242b33b7823dfb5e6b36380
EzhNEEsh <oyaiskander@yandex.ru>
2022-12-13 18:13:36 +0300

    CORS?

--- hostel/hostel/settings.py
+++ hostel/hostel/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    #'users',
+    'corsheaders',
     'authentication',
     'authentication.users',
     'dormitory',
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +59,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://*',
 ]
 
 ROOT_URLCONF = 'hostel.urls'
