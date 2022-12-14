033ce262db9384a672be474c91e05ce3f9a81050
EzhNEEsh <oyaiskander@yandex.ru>
2022-12-14 09:52:40 +0300

    V1.0.0

--- hostel/hostel/urls.py
+++ hostel/hostel/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('api/', include('rooms.urls')),
     path('api/', include('settlers.urls')),
     path('api/', include('students.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- hostel/students/urls.py
+++ hostel/students/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 from .views import StudentsView, StudentsPkView
 
@@ -11,4 +9,4 @@ app_name = 'authentication'
 urlpatterns = [
     path('dormitory/<int:dormitory_pk>/students', StudentsView.as_view()),
     path('dormitory/<int:dormitory_pk>/students/<int:pk>', StudentsPkView.as_view())
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
